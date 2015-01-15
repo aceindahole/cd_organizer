f18d65e664093ad486ea55b3f22111b511e69245
Tyler Jeffrey Brown & Alex Kaufman <student@epicodus.com>
2015-01-15 10:10:33 -0800

    Add new specs and methods

--- lib/album.rb
+++ lib/album.rb
@@ -1,7 +1,17 @@
 class Album
   attr_reader(:album_name)
+  @@albums = []
 
   define_method(:initialize) do |album_name|
     @album_name = album_name
   end
+
+  define_singleton_method(:all) do
+    @@albums
+  end
+
+  define_method(:save) do
+    @@albums.push(self)
+  end
+
 end
--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -2,10 +2,25 @@ require('rspec')
 require('album')
 
 describe(Album) do
-  describe('#name') do
+  describe('#album_name') do
     it('returns the name of the album') do
       test_album = Album.new("The White Album")
-      expect(test_album.name()).to(ea("The White Album"))
+      expect(test_album.album_name()).to(eq("The White Album"))
     end
   end
+
+  describe('.all') do
+    it('is empty before putting a album name in') do
+      expect(Album.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves the album name to the array of saved albums') do
+      test_album = Album.new("The White Album")
+      test_album.save()
+      expect(Album.all()).to(eq([test_album]))
+    end
+  end
+
 end
